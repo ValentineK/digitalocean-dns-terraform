bd7aee1bf842d1e24ad1e641d5e089a0343e25d5
Valentin Konov <valentin.konov@experteer.com>
2022-06-05 00:16:45 +0200

    remove modules init

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ resource "digitalocean_domain" "domain" {
   name = var.domain_name
 }
 
-module "google" {
-  source      = "./modules/google"
-  domain_name = var.domain_name
-  count       = var.google_enable ? 1 : 0
-
-  depends_on  = [ digitalocean_domain.domain ]
-}
-
-module "apple" {
-  source      = "./modules/apple"
-  domain_name = var.domain_name
-  count       = var.apple_enable ? 1 : 0
-
-  depends_on  = [ digitalocean_domain.domain ]
-}
-
 resource "digitalocean_record" "txt" {
   for_each = {for index, prop in var.txt_records:  sha1("${prop.name}/${prop.target}") => prop}
   domain   = digitalocean_domain.domain.name
